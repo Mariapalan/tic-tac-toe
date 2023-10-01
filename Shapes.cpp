86f2b9cac9104584332a019238986f3108203885
mariabalanjian <maria.balanjian@gmail.com>
2023-10-01 16:10:48 -0400

    Initial commit

---
+++ Shapes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Maria Balanjian on 2023-10-01.
+//
+
+#include "Shapes.h"
---
+++ Shapes.h
@@ -0,0 +1,8 @@
+//
+// Created by Maria Balanjian on 2023-10-01.
+//
+
+#ifndef UNTITLED_SHAPES_H
+#define UNTITLED_SHAPES_H
+
+#endif //UNTITLED_SHAPES_H
